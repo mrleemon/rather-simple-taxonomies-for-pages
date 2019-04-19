99c1b76092171dbc521ddf58483da7ea50f3473f
leemon <tacrapo@gmail.com>
2019-04-19 14:36:45 +0200

    First commit

---
+++ readme.txt
@@ -0,0 +1,24 @@
+=== Really Simple Taxonomies for Pages ===
+Contributors: leemon
+Tags: pages, taxonomies, categories, tags
+Requires at least: 4.0
+Tested up to: 4.5
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+ Adds exclusive categories and tags for pages.
+
+== Description ==
+
+ Adds exclusive categories and tags for pages.
+
+This plugin is experimental and is provided with no support or warranty. 
+
+== Installation ==
+1. Upload the extracted plugin folder and contained files to your /wp-content/plugins/ directory
+2. Activate the plugin through the "Plugins" menu in WordPress
+
+== Changelog ==
+= 1.0 =
+* Initial release
---
+++ really-simple-taxonomies-for-pages.php
@@ -0,0 +1,135 @@
+<?php
+/*
+Plugin Name: Really Simple Taxonomies for Pages
+Plugin URI: 
+Description: Adds exclusive categories and tags for pages.
+Version: 1.0
+Author: Oscar Ciutat
+Author URI: http://oscarciutat.com/code/
+Text Domain: really-simple-taxonomies-for-pages
+License: GPLv2 or later
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as 
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+class Really_Simple_Taxonomies_For_Pages {
+	
+	/**
+	 * Plugin instance.
+	 *
+	 * @see get_instance()
+	 * @type object
+	 */
+	protected static $instance = null;
+
+
+	/**
+	 * Access this plugin’s working instance
+	 *
+	 * @wp-hook plugins_loaded
+	 * @return  object of this class
+	 */
+	public static function get_instance() {
+		
+		if ( !self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+
+	}
+
+	
+	/**
+	 * Used for regular plugin work.
+	 *
+	 * @wp-hook plugins_loaded
+	 * @return  void
+	 */
+	public function plugin_setup() {
+		
+		// Init
+		add_action( 'init', array( $this, 'load_language' ) );
+		add_action( 'init', array( $this, 'register_taxonomy' ) );
+
+	}
+	
+	
+	/**
+	 * Constructor. Intentionally left empty and public.
+	 *
+	 * @see plugin_setup()
+	 */
+	public function __construct() {}
+
+	
+	/*
+	 * load_language
+	 *
+	 * @since 1.0
+	 */
+    function load_language() {
+		load_plugin_textdomain( 'really-simple-taxonomies-for-pages', '', dirname(plugin_basename( __FILE__ )) . '/languages/' );
+    }
+
+    
+    /*
+	 * register_taxonomy
+	 *
+	 * @since 1.0
+	 */
+    function register_taxonomy() {
+        
+        $labels = array(
+            'name'                       => _x( 'Categories', 'taxonomy general name' ),
+            'singular_name'              => _x( 'Category', 'taxonomy singular name' ),
+            'search_items'               => __( 'Search Categories' ),
+            'all_items'                  => __( 'All Categories' ),
+            'parent_item'                => __( 'Parent Category' ),
+            'parent_item_colon'          => __( 'Parent Category:' ),
+            'edit_item'                  => __( 'Edit Category' ),
+            'view_item'                  => __( 'View Category' ),
+            'update_item'                => __( 'Update Category' ),
+            'add_new_item'               => __( 'Add New Category' ),
+            'new_item_name'              => __( 'New Category Name' ),
+            'not_found'                  => __( 'No categories found.' ),
+            'no_terms'                   => __( 'No categories' ),
+            'items_list_navigation'      => __( 'Categories list navigation' ),
+            'items_list'                 => __( 'Categories list' ),
+            'most_used'                  => _x( 'Most Used', 'categories' ),
+            'back_to_items'              => __( '&larr; Back to Categories' ),
+        );
+
+        $args = array(
+            'labels' => $labels,
+            'hierarchical' => true,
+            'public' => true,
+            'show_ui' => true,
+            'show_in_rest' => true,
+            'show_admin_column' => true,
+            'show_in_nav_menus' => true,
+            'query_var' => true,
+            'rewrite' => array(
+                'slug' => 'page_category',
+                'with_front' => false,
+                'hierarchical' => true
+            )
+        );
+		
+        register_taxonomy( 'page_category', 'page', $args );
+    }
+
+}
+
+add_action( 'plugins_loaded', array ( Really_Simple_Taxonomies_For_Pages::get_instance(), 'plugin_setup' ) );
